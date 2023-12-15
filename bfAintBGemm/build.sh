6cefa727ef4afda5cb68d933bd0af0374dd42c2a
shaojiewang <wsjmessi@163.com>
2023-12-15 21:52:58 +0800

    add asm build command

--- bfAintBGemm/build.sh
+++ bfAintBGemm/build.sh
@@ -8,3 +8,8 @@ rm -rf $BUILD ; mkdir $BUILD ; cd $BUILD
 
 /opt/rocm/bin/hipcc $TOP/$SRC -fPIC -std=c++17 -O3 -Wall --offload-arch=gfx90a -save-temps -o $BUILD/$OUT
 
+KSRC=bf16gemm_kernel_gfx90a.s
+KOUT=bf16gemm_kernel_gfx90a.hsaco
+
+/opt/rocm/llvm/bin/clang++ -x assembler -target amdgcn--amdhsa -mcpu=gfx90a $KSRC -o $KOUT
+
