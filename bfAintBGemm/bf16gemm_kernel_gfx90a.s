daf4bcc3a8290c95e0c29c527a68097595f92c1c
shaojiewang <wsjmessi@163.com>
2024-01-11 14:12:30 +0800

    begin 32x128x32 t256

--- bfAintBGemm/bf16gemm_kernel_gfx90a.s
+++ bfAintBGemm/bf16gemm_kernel_gfx90a.s
@@ -179,10 +179,10 @@
 .set v_tid,             128
 
 .text
-.global bf16gemm_rrr_wg1x1_w1x2_32x32x8bf16_1k_pregld1
+.global bf16gemm_rrr_wg_32x128x32_wg1x1_w1x4_32x32x8bf16_1k_pregld1
 .p2align 8
-.type bf16gemm_rrr_wg1x1_w1x2_32x32x8bf16_1k_pregld1,@function
-bf16gemm_rrr_wg1x1_w1x2_32x32x8bf16_1k_pregld1:
+.type bf16gemm_rrr_wg_32x128x32_wg1x1_w1x4_32x32x8bf16_1k_pregld1,@function
+bf16gemm_rrr_wg_32x128x32_wg1x1_w1x4_32x32x8bf16_1k_pregld1:
     ; http://www.hsafoundation.com/html/Content/Runtime/Topics/02_Core/hsa_kernel_dispatch_packet_t.htm
 
     s_load_dwordx2 s[s_ptr_c:s_ptr_c+1], s[s_ka:s_ka+1], 0+k_ptr_c
@@ -635,7 +635,7 @@ label_write_out_c:
 
 .rodata
 .p2align 6
-.amdhsa_kernel bf16gemm_rrr_wg1x1_w1x2_32x32x8bf16_1k_pregld1
+.amdhsa_kernel bf16gemm_rrr_wg_32x128x32_wg1x1_w1x4_32x32x8bf16_1k_pregld1
     .amdhsa_group_segment_fixed_size 16384
     .amdhsa_user_sgpr_dispatch_ptr 0
     .amdhsa_user_sgpr_kernarg_segment_ptr 1
@@ -655,8 +655,8 @@ label_write_out_c:
 ---
 amdhsa.version: [ 1, 0 ]
 amdhsa.kernels:
-  - .name: bf16gemm_rrr_wg1x1_w1x2_32x32x8bf16_1k_pregld1
-    .symbol: bf16gemm_rrr_wg1x1_w1x2_32x32x8bf16_1k_pregld1.kd
+  - .name: bf16gemm_rrr_wg_32x128x32_wg1x1_w1x4_32x32x8bf16_1k_pregld1
+    .symbol: bf16gemm_rrr_wg_32x128x32_wg1x1_w1x4_32x32x8bf16_1k_pregld1.kd
     .sgpr_count: 79
     .vgpr_count: 129
     .kernarg_segment_align: 8
