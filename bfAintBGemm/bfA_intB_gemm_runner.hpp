c2ddf2a9c1724fce0b6f79783cb1467c8516cdfa
shaojiewang <wsjmessi@163.com>
2024-02-21 21:06:37 +0800

    add kernel runner

---
+++ bfAintBGemm/bfA_intB_gemm_runner.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+class bfAintBGemmRunner {
+public:
+    bfAintBGemmRunner(const kernel_tunable* k_list)
+    {
+    }
+ 
+    void run(kernel_tunable& kernel,
+             args& k_args,
+             hipStream_t ss)
+    {
+        
+    }
+    
+    ~bfAintBGemmRunner() = default;    
+
+};
