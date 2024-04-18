f7b45e7217d5a77d10630c3ed5b1f7080379abc0
shaojiewang <wsjmessi@163.com>
2024-04-18 09:53:13 +0000

    add llama 175b test

--- bfAintBGemm/script/llama_bfAintB_gemm_benchmark.sh
+++ bfAintBGemm/script/llama_bfAintB_gemm_benchmark.sh
@@ -1,7 +1,9 @@
 #/bin/bash
 
-for m in 1 1024 2048; do
-    
+for m in 1 2048 32768; do
+   
+if [ 0 = 1 ];
+then 
     echo "llama 13B tp 1"
     ./build/bfAintBGemm.exe ./build 0 1 $m 5120 5120
     ./build/bfAintBGemm.exe ./build 0 1 $m 15360 5120
@@ -13,6 +15,13 @@ for m in 1 1024 2048; do
     ./build/bfAintBGemm.exe ./build 0 1 $m 8192 2048
     ./build/bfAintBGemm.exe ./build 0 1 $m 11008 8192
     ./build/bfAintBGemm.exe ./build 0 1 $m 8192 5504
+fi
+
+    echo "llama 175B tp 8"
+    ./build/bfAintBGemm.exe ./build 0 1 $m 4608 12288
+    ./build/bfAintBGemm.exe ./build 0 1 $m 12288 1536
+    ./build/bfAintBGemm.exe ./build 0 1 $m 8192 12288
+    ./build/bfAintBGemm.exe ./build 0 1 $m 12288 4096
 
 done
 
