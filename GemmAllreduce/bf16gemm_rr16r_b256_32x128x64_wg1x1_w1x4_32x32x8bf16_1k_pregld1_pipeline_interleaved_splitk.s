9a2c74e3a5ef5d27d81b7791a32a70082ad254b9
shaojiewang <wsjmessi@163.com>
2024-06-17 04:28:03 +0000

    add some load barrier code

--- GemmAllreduce/bf16gemm_rr16r_b256_32x128x64_wg1x1_w1x4_32x32x8bf16_1k_pregld1_pipeline_interleaved_splitk.s
+++ GemmAllreduce/bf16gemm_rr16r_b256_32x128x64_wg1x1_w1x4_32x32x8bf16_1k_pregld1_pipeline_interleaved_splitk.s
@@ -106,6 +106,7 @@
 .set k_world_barrier, 76
 .set k_local_out, 84
 .set k_peer_comm_buffer, 92
+.set k_local_rank, 100
 
 ;sgpr
 .set s_ka, 0
@@ -142,7 +143,8 @@
 .set s_k_idx, 57
 .set s_offset_local_flag, 58
 .set s_flag, 59
-.set s_flag_checker, 60  
+.set s_flag_checker, 60
+.set s_local_rank, 62  
 .set s_tmp, 64
 
 ;vgpr
@@ -193,7 +195,7 @@
 .set v_wave_id, 107
 .set v_tmp, 108
 ; local flag (can reuse the a/b/c vgprs)
-.set v_one, 0
+.set v_imm, 0
 .set v_flag, 1
 .set v_flag_load, 2
 .set v_offset_flag, 3
@@ -215,6 +217,7 @@ bf16gemm_rr16r_b256_32x128x64_wg1x1_w1x4_32x32x8bf16_1k_pregld1_pipeline_interle
     s_load_dwordx2 s[s_world_barrier:s_world_barrier+1], s[s_ka:s_ka+1], 0+k_world_barrier
     s_load_dwordx2 s[s_local_out:s_local_out+1], s[s_ka:s_ka+1], 0+k_local_out
     s_load_dwordx2 s[s_peer_comm_buffer:s_peer_comm_buffer+1], s[s_ka:s_ka+1], 0+k_peer_comm_buffer
+    s_load_dwordx2 s[s_local_rank:s_local_rank+1], s[s_ka:s_ka+1], 0+k_local_rank
 
     s_load_dwordx4 s[s_m:s_m+3], s[s_ka:s_ka+1], 0+k_m
     s_load_dwordx2 s[s_ldb:s_ldb+1], s[s_ka:s_ka+1], 0+k_ldb
@@ -761,12 +764,12 @@ label_write_out_c:
 
 l_local_compute_signal:
     ; find proper flag
-    v_mov_b32 v[v_one], 1
+    v_mov_b32 v[v_imm], 1
     s_lshr_b32 s[s_offset_local_flag], s[s_bx], 2
     s_lshl_b32 s[s_offset_local_flag], s[s_offset_local_flag], 2
-    v_cmpx_gt_u32 v[v_one], v[v_tid]
     v_mov_b32 v[v_offset_flag], 0
-    global_atomic_add v[v_flag], v[v_offset_flag], v[v_one], s[s_local_flag : s_local_flag + 1] glc
+    v_cmpx_gt_u32 v[v_imm], v[v_tid]
+    global_atomic_add v[v_flag], v[v_offset_flag], v[v_imm], s[s_local_flag : s_local_flag + 1] glc
     s_add_u32 s[s_flag_checker], s[s_m], 31
     s_lshr_b32 s[s_flag_checker], s[s_flag_checker], 5
     s_lshl_b32 s[s_flag_checker], s[s_flag_checker], 2
@@ -776,6 +779,12 @@ l_local_compute_signal:
     s_cbranch_scc0 l_end_bf16gemm_rr16r_b256_32x128x64_wg1x1_w1x4_32x32x8bf16_1k_pregld1_pipeline_interleaved_splitk
 
     ; begin multicard barrier
+    s_mov_b64 exec -1
+    v_mov_b32 v[v_imm], 4
+    v_cmpx_ge_u32 v[v_imm], v[v_tid]
+    v_lshlrev_b32 v[v_barrier_offset], 3, v[v_tid]
+    global_load_dwordx2 v[v_barrier_addr : v_barrier_addr + 1], v[v_barrier_offset], s[s_world_barrier : s_world_barrier + 1] off
+    
 
     .print v_flag, s_print, s_bx, v_tid, v_tmp + 7
     global_store_dword v[v_offset_flag], v[v_offset_flag], s[s_local_flag : s_local_flag + 1] glc
