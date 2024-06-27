5d7baad79caed0b04a251c8304d9df3122fd8ce8
shaojiewang <wsjmessi@163.com>
2024-06-27 14:48:33 +0000

    runable; but not right

--- GemmAllreduce/bf16gemm_rr16r_b256_32x128x64_wg1x1_w1x4_32x32x8bf16_1k_pregld1_pipeline_interleaved_splitk.s
+++ GemmAllreduce/bf16gemm_rr16r_b256_32x128x64_wg1x1_w1x4_32x32x8bf16_1k_pregld1_pipeline_interleaved_splitk.s
@@ -858,49 +858,53 @@ l_begin_barrier_check:
         .i_acc_ar = .i_acc_ar+1
     .endr
 
-l_loop_multigpu_reduce_begin:
-    
     v_add_u32 v[v_c_buffer_offset], v[v_c_buffer_offset], s[s_block_offset]
+    
+l_loop_multigpu_reduce_begin:
     
     global_load_dword v[v_peer], v[v_c_buffer_offset], s[s_peer_comm_buff_ptr : s_peer_comm_buff_ptr + 1] glc
     global_load_dword v[v_peer + 1], v[v_c_buffer_offset], s[s_peer_comm_buff_ptr + 2 : s_peer_comm_buff_ptr + 3] offset: 0
     global_load_dword v[v_peer + 2], v[v_c_buffer_offset], s[s_peer_comm_buff_ptr + 4 : s_peer_comm_buff_ptr + 5] offset: 0
-    global_load_dword v[v_peer + 3], v[v_c_buffer_offset], s[s_peer_comm_buff_ptr + 6 : s_peer_comm_buff_ptr + 7] offset: 0
+    global_load_dword v[v_peer + 3], v[v_c_buffer_offset], s[s_peer_comm_buff_ptr + 6 : s_peer_comm_buff_ptr + 7] offset: 0    
 
     s_lshl_b32 s[s_loop_step], s[s_n], 1
     v_add_u32 v[v_c_buffer_offset], v[v_c_buffer_offset], s[s_loop_step]
     
     s_waitcnt vmcnt(3)
     v_lshlrev_b32 v[v_tmp], 16, v[v_peer]
-    v_and_b32 v[v_tmp + 1], v[v_peer], 0xffff0000
-    v_add_f32 v[v_acc_all_reduce], v[v_tmp], v[v_acc_all_reduce]
+    v_and_b32 v[v_tmp + 1], 0xffff0000, v[v_peer]
+    v_add_f32 v[v_acc_all_reduce + 0], v[v_tmp + 0], v[v_acc_all_reduce + 0]
     v_add_f32 v[v_acc_all_reduce + 1], v[v_tmp + 1], v[v_acc_all_reduce + 1]
     
     s_waitcnt vmcnt(2)
     v_lshlrev_b32 v[v_tmp], 16, v[v_peer + 1]
-    v_and_b32 v[v_tmp + 1], v[v_peer + 1], 0xffff0000
-    v_add_f32 v[v_acc_all_reduce + 2], v[v_tmp], v[v_acc_all_reduce + 2]
-    v_add_f32 v[v_acc_all_reduce + 3], v[v_tmp + 1], v[v_acc_all_reduce + 3]
+    v_and_b32 v[v_tmp + 1], 0xffff0000, v[v_peer + 1], 
+    v_add_f32 v[v_acc_all_reduce + 0], v[v_tmp + 0], v[v_acc_all_reduce + 0]
+    v_add_f32 v[v_acc_all_reduce + 1], v[v_tmp + 1], v[v_acc_all_reduce + 1]
     
     s_waitcnt vmcnt(1)
     v_lshlrev_b32 v[v_tmp], 16, v[v_peer + 2]
-    v_and_b32 v[v_tmp + 1], v[v_peer + 2], 0xffff0000
-    v_add_f32 v[v_acc_all_reduce + 4], v[v_tmp], v[v_acc_all_reduce + 4]
-    v_add_f32 v[v_acc_all_reduce + 5], v[v_tmp + 1], v[v_acc_all_reduce + 5]
+    v_and_b32 v[v_tmp + 1], 0xffff0000, v[v_peer + 2]
+    v_add_f32 v[v_acc_all_reduce + 0], v[v_tmp + 0], v[v_acc_all_reduce + 0]
+    v_add_f32 v[v_acc_all_reduce + 1], v[v_tmp + 1], v[v_acc_all_reduce + 1]
    
     s_waitcnt vmcnt(0)
     v_lshlrev_b32 v[v_tmp], 16, v[v_peer + 3]
-    v_and_b32 v[v_tmp + 1], v[v_peer + 3], 0xffff0000
-    v_add_f32 v[v_acc_all_reduce + 6], v[v_tmp], v[v_acc_all_reduce + 6]
-    v_add_f32 v[v_acc_all_reduce + 7], v[v_tmp + 1], v[v_acc_all_reduce + 7]
+    v_and_b32 v[v_tmp + 1], 0xffff0000, v[v_peer + 3]
+    v_add_f32 v[v_acc_all_reduce + 0], v[v_tmp + 0], v[v_acc_all_reduce + 0]
+    v_add_f32 v[v_acc_all_reduce + 1], v[v_tmp + 1], v[v_acc_all_reduce + 1]
  
+    s_mov_b64 exec -1
+    v_pack_b32_f16 v[v_acc_all_reduce + 0], v[v_acc_all_reduce + 0], v[v_acc_all_reduce + 1], op_sel: [1, 1] 
+    global_store_dword v[v_c_buffer_offset], v[v_acc_all_reduce + 0], s[s_local_out : s_local_out + 1] offset: 0
+
     s_mul_i32 s[s_reduce_range], s[s_m], s[s_loop_step]
     v_cmp_le_u32 vcc, s[s_reduce_range], v[v_c_buffer_offset]
     s_andn2_b64 exec, exec, vcc
     s_cbranch_execnz l_loop_multigpu_reduce_begin
     
-    s_mov_b64 exec -1
-    
+
+     
 
 l_end_barrier_check:
     v_mov_b32 v[v_imm], 4
