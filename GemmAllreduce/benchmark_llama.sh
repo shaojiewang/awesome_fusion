3d290e9e942ef087ff69649b347e3f6203647045
shaojiewang <wsjmessi@163.com>
2024-06-30 15:07:41 +0000

    not reset local flag nor barrier to zero

--- GemmAllreduce/benchmark_llama.sh
+++ GemmAllreduce/benchmark_llama.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # mpirun -n 4 --allow-run-as-root build/test_gemm_ar 1 8192 8192 4 0
-mpirun -n 4 --allow-run-as-root build/test_gemm_ar 2 512 8192 4 0
+mpirun -n 4 --allow-run-as-root build/test_gemm_ar 16 8192 8192 4 0
 # mpirun -n 4 --allow-run-as-root build/test_gemm_ar 32 8192 8192 4 0
 
