11ae278b9532fe873d61e8a2abe33a97e74c4576
shaojiewang <wsjmessi@163.com>
2024-07-09 12:57:21 +0000

    test time divided

--- GemmAllreduce/gemmAR.cc
+++ GemmAllreduce/gemmAR.cc
@@ -468,7 +468,7 @@ int gemm_ar(const test_args_t& args, const int& rank, const int& world_size)
     for(int i = 0; i < TOTAL_NUM; i++)
     {
         bfa_intb_gemm_runner.run(bfa_intb_gemm_runner.k_ptr[sol_idx_no_fuse], bfa_intb_gemm_runner.kernel_func_vec[sol_idx_no_fuse], compute_stream, sk_blocks_no_fuse);
-        // custom_all_reduce_comms[rank]->customAllReduce(m * n, compute_stream);
+        custom_all_reduce_comms[rank]->customAllReduce(m * n, compute_stream);
     }
 
     check_cuda_error(hipEventRecord(evt_no_fuse_end, compute_stream));
@@ -494,7 +494,71 @@ int gemm_ar(const test_args_t& args, const int& rank, const int& world_size)
         printf("\n");
     }
 
-    return 0;
+    float elapsed_ms_gemm;
+    check_cuda_error(hipEventCreate(&evt_no_fuse_start));
+    check_cuda_error(hipEventCreate(&evt_no_fuse_end));
+    check_cuda_error(hipDeviceSynchronize());
+    check_cuda_error(hipEventRecord(evt_no_fuse_start, compute_stream));
+
+    for(int i = 0; i < TOTAL_NUM; i++)
+    {
+        bfa_intb_gemm_runner.run(bfa_intb_gemm_runner.k_ptr[sol_idx_no_fuse], bfa_intb_gemm_runner.kernel_func_vec[sol_idx_no_fuse], compute_stream, sk_blocks_no_fuse);
+    }
+    check_cuda_error(hipEventRecord(evt_no_fuse_end, compute_stream));
+    check_cuda_error(hipEventSynchronize(evt_no_fuse_end));
+    check_cuda_error(hipDeviceSynchronize());
+    check_cuda_error(hipEventElapsedTime(&elapsed_ms_gemm, evt_no_fuse_start, evt_no_fuse_end));
+    check_cuda_error(hipEventDestroy(evt_no_fuse_start));
+    check_cuda_error(hipEventDestroy(evt_no_fuse_end));
+
+    {
+        float time_per_loop = elapsed_ms_gemm / TOTAL_NUM;
+        float tflops = (float)2 * m * n * k_per_card / time_per_loop / (1024 * 1024 * 1024);
+        float bw_gbs = (float)(2 * (m * k_per_card + m * n) + n * k_per_card) / time_per_loop / (1024 * 1024);
+    
+        printf("origin impl gemm [sol, sk_blocks]: [%d, %d], m: %d, n: %d, k: %d, time: %.3f ms, tflops: %.3f, bw: %.3f GB/s\n",
+            sol_idx_no_fuse, sk_blocks_no_fuse,
+            m,
+            n,
+            k_per_card,
+            time_per_loop,
+            tflops,
+            bw_gbs);
+        printf("\n");
+    }
+
+    float elapsed_ms_comm;
+    check_cuda_error(hipEventCreate(&evt_no_fuse_start));
+    check_cuda_error(hipEventCreate(&evt_no_fuse_end));
+    check_cuda_error(hipDeviceSynchronize());
+    check_cuda_error(hipEventRecord(evt_no_fuse_start, compute_stream));
+
+    for(int i = 0; i < TOTAL_NUM; i++)
+    {
+        custom_all_reduce_comms[rank]->customAllReduce(m * n, compute_stream);
+    }
+    check_cuda_error(hipEventRecord(evt_no_fuse_end, compute_stream));
+    check_cuda_error(hipEventSynchronize(evt_no_fuse_end));
+    check_cuda_error(hipDeviceSynchronize());
+    check_cuda_error(hipEventElapsedTime(&elapsed_ms_comm, evt_no_fuse_start, evt_no_fuse_end));
+    check_cuda_error(hipEventDestroy(evt_no_fuse_start));
+    check_cuda_error(hipEventDestroy(evt_no_fuse_end));
+
+    {
+        float time_per_loop = elapsed_ms_comm / TOTAL_NUM;
+        float tflops = (float)2 * m * n * k_per_card / time_per_loop / (1024 * 1024 * 1024);
+        float bw_gbs = (float)(2 * (m * k_per_card + m * n) + n * k_per_card) / time_per_loop / (1024 * 1024);
+    
+        printf("origin impl comm [sol, sk_blocks]: [%d, %d], m: %d, n: %d, k: %d, time: %.3f ms, tflops: %.3f, bw: %.3f GB/s\n",
+            sol_idx_no_fuse, sk_blocks_no_fuse,
+            m,
+            n,
+            k_per_card,
+            time_per_loop,
+            tflops,
+            bw_gbs);
+        printf("\n");
+    }
 
     check_cuda_error(hipDeviceSynchronize());
     MPI_Barrier(MPI_COMM_WORLD);
